28c6c7e8b0be3edf482ea6f69d390495609f9464
Om Majithiya <omajethiya1@gmail.com>
2023-11-08 18:10:07 +0530

    submit

---
+++ app/src/main/java/com/example/redsquare/StartPage.java
@@ -0,0 +1,38 @@
+package com.example.redsquare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.ClosedSubscriberGroupInfo;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class StartPage extends AppCompatActivity {
+
+    EditText username,password;
+    Button loginBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_page);
+
+        username=(EditText)findViewById(R.id.username);
+        password=(EditText) findViewById(R.id.password);
+        loginBtn=(Button) findViewById(R.id.loginBtn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (username.getText().toString().equals("om")&&password.getText().toString().equals("1234")){
+                    startActivity(new Intent(getApplicationContext(), Registration.class));
+                } else {
+                    Toast.makeText(getApplicationContext(), "Login Fail", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+    }
+}
